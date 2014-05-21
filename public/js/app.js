e5513e96ea551694de2f82c82ce0f80b8dc33f86
Brock Whitten <brock@chloi.io>
2014-05-21 10:36:59 -0500

    adds year dropdown

--- public/js/app.js
+++ public/js/app.js
@@ -8,11 +8,22 @@ function MyController($scope, $http) {
     { name:'kings' }
   ];
 
+  $scope.seasons = [
+    { years: "20132014" },
+    { years: "20122013" },
+    { years: "20112012" },
+    { years: "20102011" },
+    { years: "20092010" },
+    { years: "20082009" }
+  ]
+
   $scope.players = []
+
   $scope.team = $scope.teams[2];
+  $scope.season = $scope.seasons[0];
 
   $scope.change = function(){
-    $http.get("http://nhl.chloi.io/" + $scope.team.name).success(function(players){
+    $http.get("http://nhl.chloi.io/" + $scope.team.name +"/"+ $scope.season.years).success(function(players){
       console.log(JSON.stringify(players))
       $scope.players = players;
     })
