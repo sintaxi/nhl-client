1bea5222e3fa62bd402cc1bbfb67569382f305bb
Brock Whitten <brock@chloi.io>
2014-05-21 09:47:24 -0500

    changes endpoint

--- public/js/app.js
+++ public/js/app.js
@@ -2,16 +2,17 @@
 function MyController($scope, $http) {
 
   $scope.teams = [
-    { name:'oilers', shade:'dark' },
-    { name:'flames', shade:'light' },
-    { name:'blackhawks', shade:'light' }
+    { name:'oilers' },
+    { name:'flames' },
+    { name:'blackhawks' },
+    { name:'kings' }
   ];
 
   $scope.players = []
   $scope.team = $scope.teams[2];
 
   $scope.change = function(){
-    $http.get("http://localhost:9001/" + $scope.team.name).success(function(players){
+    $http.get("http://nhl.chloi.io/" + $scope.team.name).success(function(players){
       console.log(JSON.stringify(players))
       $scope.players = players;
     })
